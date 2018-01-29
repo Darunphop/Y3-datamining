76aa11d726fd808c151d4d271bfd9b9c608da22b
darunphop <darunphop.p@gmail.com>
2018-01-29 23:17:42 +0700

    befor cleaning

--- hw1/hw1.3.py
+++ hw1/hw1.3.py
@@ -2,6 +2,7 @@ import bokeh
 import codecs
 import numpy as np
 import seaborn as sns
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import pandas as pd
 
@@ -32,21 +33,28 @@ def preprocess(raw):
             data.append(-1)
     return data
 
-def ploting(dataset):
-    sns.distplot(dataset)
-    plt.show()
+def ploting(data):
+    ax = plt.scatter(data["index"], data[13],s=5)
+    plt.xticks(np.arange(min(data["index"]), max(data["index"]), 50))
 
 if __name__=="__main__":
-    # data = preprocess(read_file())
-    # for i in data:
-    #     print(i)
-    # ploting(data)
-
     data = pd.read_csv('HW1DATA.csv', header=None, na_values='?')
     data['index'] = np.arange(1, len(data)+1)
 
-    print(data[13])
-    sns.pointplot(x="index", y=13, data=data);
+    unknow_index = data.loc[data[13].isnull()]['index'].values
+
+    # sns.stripplot(unknow_index,linewidth=3,size=0.5)
+
+    ploting(data)
+
+    # sns.pointplot(x="index", y=13, data=data, markers='o', size=0.8,linewidth=0)
+    # ax = sns.stripplot(x="index", y=13, data=data, size=0.8,linewidth=2)
+    # ax.get_xaxis().set_visible(False)
+    # sns.distplot(data[13].dropna())
+    # sns.boxplot(x=13, data=data)
     # sns.pointplot()
     # sns.pointplot(data[13].dropna())
+
+
+    plt.grid(alpha=0.5)
     plt.show()
