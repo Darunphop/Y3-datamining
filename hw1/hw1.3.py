deb1f5e0abfc1bfb86324f79d36389588a1d152a
darunphop <darunphop.p@gmail.com>
2018-01-30 16:11:40 +0700

    mostly finished

--- hw1/hw1.3.py
+++ hw1/hw1.3.py
@@ -13,8 +13,8 @@ def preprocess(raw):
     raw[13][ raw[13] > threshole_val] = np.nan
     where_are_NaNs = np.isnan(raw[13])
     raw["index"][where_are_NaNs] = np.nan
-    print(md)
-    print(threshole_val)
+    # print(md)
+    # print(threshole_val)
     return raw
 
 def ploting(data,na_data,showNA=True):
@@ -36,33 +36,46 @@ def lineplot(data):
     ax = fig.add_subplot(111)
     sns.pointplot(x="index", y=13, data=data, markers='o', size=0.8,linewidth=0, scale=0.4)
 
-def rangeplot(data,middle,rangew):
-    fig = plt.figure(3)
-    ax = fig.add_subplot(111)
-    for i in range(0,1):
-        print(middle[i])
-        print(data[13][middle[i]-rangew:middle[i]+rangew]-1)
-        y = data[13][middle[i]-rangew:middle[i]+rangew].dropna()
-        x = data["index"][middle[i]-rangew:middle[i]+rangew].dropna()
+def rangeplot(data,middle,window):
+    reg_f = []
+    for i in range(0,len(middle)):
+    # for i in range(0,1):
+        fig = plt.figure(middle[i])
+        ax = fig.add_subplot(111)
+        # print(middle[i])
+        # print(data[13][middle[i]-window:middle[i]+window])
+        y = data[13][middle[i]-window:middle[i]+window].dropna()
+        x = data["index"][middle[i]-window:middle[i]+window].dropna()
 
-        ax = sns.regplot(x=x, y=y, order=4, ci=None, truncate=True)
+        reg_f.append(np.polyfit(x, y, 4))
+        # print(data[13][middle[i]])
+        new_point = np.polyval(reg_f[i],middle[i])
+        data[13][middle[i]-1] = new_point
 
-    return
+        
+
+        ax.scatter(middle[i], new_point,s=50,c='r')
+        
+
+        sns.regplot(x=x, y=y, order=4, ci=None, truncate=True)
+
+        plt.grid(alpha=0.5)
+    return reg_f
 
 if __name__=="__main__":
     data = pd.read_csv('HW1DATA.csv', header=None, na_values='?')
     data['index'] = np.arange(1, len(data)+1)
 
     unknow_index = data.loc[data[13].isnull()]['index'].values
-
+    
     data = preprocess(data)
     # sns.stripplot(unknow_index,linewidth=3,size=0.5)
 
     # ploting(data,unknow_index,False)
     # boxplot(data)
     # lineplot(data)
-    
-    rangeplot(data,unknow_index,10)
+
+    reg_f = rangeplot(data,unknow_index,10)
 
     # ax = sns.stripplot(x="index", y=13, data=data, size=0.8,linewidth=2)
     # ax.get_xaxis().set_visible(False)
@@ -75,8 +88,15 @@ if __name__=="__main__":
 
     # ax = sns.regplot(x=data["index"], y=data[13], data=data, order=3, ci=None, truncate=True)
 
-    # for i in unknow_index:
-    #     print(data[13][i-1], i)
+    for i in unknow_index:
+        print(data[13][i-1], i)
 
-    plt.grid(alpha=0.5)
+    # print(reg_f[0].get_lines()[0].get_xdata())
+    # print(reg_f[0].get_lines()[0].get_ydata())
+
+    k=0
+    for i in unknow_index:
+        print(i," : ",np.polyval(reg_f[k],i))
+        k += 1
+
     plt.show()
