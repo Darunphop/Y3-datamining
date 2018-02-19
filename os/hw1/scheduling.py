59c8a435de7355a393f0a83b6006f2f9c7827067
darunphop <darunphop.p@gmail.com>
2018-02-19 16:56:07 +0700

    FCFS

--- os/hw1/scheduling.py
+++ os/hw1/scheduling.py
@@ -9,10 +9,13 @@ def testSetGen(ratio):
     np.random.shuffle(result)
     return result
 
+def algoFCFS(data):
+    return data #suppose data is index by time
+
 if __name__ == "__main__":
     testSetRatio = []
     testSetRatio.append([20,20,20]) #test set 1
-    testSetRatio.append([30,0,30])  #test set 2
+    testSetRatio.append([30, 0,30])  #test set 2
     testSetRatio.append([20,20,20]) #test set 3
 
     for i in testSetRatio:
