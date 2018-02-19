dff5d78cdb35d9789c17420a0ec33c836cbd0beb
darunphop <darunphop.p@gmail.com>
2018-02-19 16:22:14 +0700

    added test set ratio

--- os/hw1/scheduling.py
+++ os/hw1/scheduling.py
@@ -5,9 +5,15 @@ testSeed = [[1,10], [15,25], [30,40]]
 def testSetGen(ratio):
     result = []
     for i in range(0,len(ratio)):
-        # print()
         result.extend(np.random.random_integers(testSeed[i][0], testSeed[i][1],ratio[i]))
+    np.random.shuffle(result)
     return result
 
 if __name__ == "__main__":
-    print(testSetGen([2,3,4]))
+    testSetRatio = []
+    testSetRatio.append([20,20,20]) #test set 1
+    testSetRatio.append([30,0,30])  #test set 2
+    testSetRatio.append([20,20,20]) #test set 3
+
+    for i in testSetRatio:
+        print(testSetGen(i))
