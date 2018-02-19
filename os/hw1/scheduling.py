0560cc49164f8f645ba6d911ee849efb5f70082c
darunphop <darunphop.p@gmail.com>
2018-02-19 18:00:00 +0700

    algoRR

--- os/hw1/scheduling.py
+++ os/hw1/scheduling.py
@@ -4,19 +4,47 @@ testSeed = [[1,10], [15,25], [30,40]]
 
 def testSetGen(ratio):
     result = []
+    tmp = []
     for i in range(0,len(ratio)):
-        result.extend(np.random.random_integers(testSeed[i][0], testSeed[i][1],ratio[i]))
-    np.random.shuffle(result)
+        tmp.extend(np.random.random_integers(testSeed[i][0], testSeed[i][1],ratio[i]))
+    np.random.shuffle(tmp)
+    for i in range(1,len(tmp)+1):
+        result.append([i,tmp[i-1]])
     return result
 
 def algoFCFS(data):
-    return data #suppose data is index by time
+    return data #suppose data is indexed by time
+
+def algoSJF(data):
+    return sorted(data, key=lambda x: x[1])
+
+def algoRR(data,qtime=10):
+    pending = []
+    result = []
+    for i in data:
+        if i[1] <= qtime:
+            result.append(i)
+        else:
+            pending.append([i[0], i[1]-qtime])
+            result.append([i[0], qtime])
+    while len(pending) > 0:
+        x = pending.pop(0)
+        if x[1] <= qtime:
+            result.append(x)
+        else:
+            pending.append([x[0], x[1]-qtime])
+            result.append([i[0], qtime])
+    return result
 
 if __name__ == "__main__":
     testSetRatio = []
     testSetRatio.append([20,20,20]) #test set 1
-    testSetRatio.append([30, 0,30])  #test set 2
-    testSetRatio.append([20,20,20]) #test set 3
+    # testSetRatio.append([30, 0,30])  #test set 2
+    # testSetRatio.append([20,20,20]) #test set 3
 
     for i in testSetRatio:
-        print(testSetGen(i))
+        test = testSetGen(i)
+        print(algoRR(test))
+        print("++++++++++++++++++++++++++")
+        print(test)
+        print("--------------------------")
