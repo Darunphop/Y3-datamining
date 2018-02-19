a8aff095dd6415afc9c59ba73cfe887a02df915c
darunphop <darunphop.p@gmail.com>
2018-02-19 15:59:59 +0700

    can gen set

--- os/hw1/scheduling.py
+++ os/hw1/scheduling.py
@@ -1,4 +1,13 @@
 import numpy as np
 
-if __name__ == "__main__"
-    print("Hi")
+testSeed = [[1,10], [15,25], [30,40]]
+
+def testSetGen(ratio):
+    result = []
+    for i in range(0,len(ratio)):
+        # print()
+        result.extend(np.random.random_integers(testSeed[i][0], testSeed[i][1],ratio[i]))
+    return result
+
+if __name__ == "__main__":
+    print(testSetGen([2,3,4]))
