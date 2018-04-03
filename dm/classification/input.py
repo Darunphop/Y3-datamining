bdbafcf2d1706b323d8aa6bd2e94987842597347
ICQCQ <darunphop.p@gmail.com>
2018-04-03 17:59:27 +0700

    base loading

--- dm/classification/input.py
+++ dm/classification/input.py
@@ -1,7 +1,16 @@
 import numpy as np
 import pandas as pd
-input_file = {1:'student-mat.scv', 2:'student-por.scv'}
+import os
+input_file = {1:'student-mat.csv', 2:'student-por.csv'}
+
+def loadFile(file_number=1):
+    if not file_number in input_file:
+        file_number = 1
+
+    fpath = os.path.join('dataset', input_file[file_number])
+    return pd.read_csv(fpath, sep=';', header=0, na_values='?')
 
 if __name__ == '__main__':
-    data = pd.read_csv(input_file[1], header=None, na_values='?')
-    print('hi')
+    data = loadFile(1)
+    print(data)
+    
