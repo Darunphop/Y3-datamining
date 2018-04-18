2abce9ea5c497734610d24250868b3832072e233
ICQCQ <darunphop.p@gmail.com>
2018-04-18 23:06:52 +0700

    come so far

--- dm/classification/neuralNetwork.py
+++ dm/classification/neuralNetwork.py
@@ -4,26 +4,58 @@ class NeuralNetwork:
     class Layer:
         def __init__(self, s, input, type='fully'):
                 self.size = s
-                self.input = []
+                self.input = input
                 self.nodes = []
-                self.type = 'fully'   # fully / direct
+                self.output = []
+                self.type = type   # fully / direct
                 self.setupNodes()
 
         def setupNodes(self):
             if self.type == 'fully':
                 for i in range(self.size):
-                    i_size = len(self.input)
-                    tmp_node = self.LinearNode(self.input, self.getRandW(i_size), self.getRandBias(i_size))
+                    i_size = len(self.getInput())
+                    rand_w = self.getRandW(i_size)
+                    rand_b = self.getRandBias()
+                    # print('rand_w', rand_w)
+                    # print('rand_b', rand_b)
+                    tmp_node = self.LinearNode(self.getInput(), rand_w, rand_b)
                     self.nodes.append(tmp_node)
+                    # print('Added fully node Successful')
             else:
                 for i in range(self.size):
-                    tmp_node = self.ActivationNode(self.input[i], 'sig')
+                    # print(self.getInput()[i])
+                    tmp_node = self.ActivationNode(self.getInput()[i], 'sig')
                     self.nodes.append(tmp_node)
+                    # print('Added Direct successful')
+            # print('setup', self.input)
 
+        def process(self):
+            tmp = []
+            for n, i in enumerate(self.nodes):
+                if self.type == 'fully':
+                    # print('ib layer process', self.getInput())
+                    i.setInput(self.getInput())
+                else:
+                    # print(self.getInput()[n])
+                    i.setInput(self.getInput()[n])
+                i.process()
+                tmp_out = i.getOutput()
+                # print('i.input',i.input)
+                # print('in layer process', tmp_out)
+                tmp.append(tmp_out)
+            self.output = tmp
 
+        def getInput(self):
+            if self.input[0].__class__ == list:
+                ip = self.input[0]
+            else:
+                ip = self.input
+            return ip
+        def getOutput(self):
+            return self.output
         def getRandW(self, size):
             return 4 * np.random.random(size) - 2   #(-2,2)
-        def getRandBias(self, size):
+        def getRandBias(self, size=1):
             return 2 * np.random.random(size) - 1   #(-1,1)
 
         class Node:
@@ -55,8 +87,13 @@ class NeuralNetwork:
 
             def getSumWeight(self):
                 sum = 0
+                # print('W',self.weight)
                 for i in range(len(self.input)):
+                    # print(i)
+                    # print('In getSumW', self.input[i])
+                    # print('In getSumW', self.weight[i])
                     sum += self.input[i] * self.weight[i]
+                # print('SUM is', sum)
                 return sum
             def setWeight(self, n_weight):
                 if len(n_weight) == len(self.input):
@@ -65,7 +102,11 @@ class NeuralNetwork:
                     raise ValueError('Weigths size not match the Input size')
 
             def process(self):
+                # print('In node BIAS', self.bias)
+                # print('In node W', self.getSumWeight())
+                # print('process bias', self.bias)
                 sumwb = self.getSumWeight() + self.bias
+                # print('sum inprocess is', sumwb)
                 super().process(sumwb)
             # END LinearNode class #
 
@@ -89,26 +130,94 @@ class NeuralNetwork:
                         return 1 - np.power(fx, 2)
             
             def process(self):
+                # print('In activate',self.getInput())
                 fx = self.activationFunction(self.getInput(), self.func)
                 super().process(fx)
             # END ActivationNode class #
 
-    data = 0
     def __init__(self):
-        self.data = 1
+        self.input = [0,0,0,0,0]
+        self.layers = []
     
+    def addHidden(self, n):
+        layer_size = len(self.layers)
+        ip = []
+        if layer_size == 0:
+            ip = self.input
+        else:
+            ip = self.layers[layer_size-1].getOutput()
+        self.addLinearLayer(n, ip)
+        ip = self.layers[layer_size].getOutput()
+        # print('IP ',ip)
+        self.addActivationLayer(n, ip)
+        self.allProcess()
+
+    def addLinearLayer(self, n, input):
+        tmp_layer = self.Layer(n, input)
+        tmp_layer.process()
+        self.layers.append(tmp_layer)
+    
+    def addActivationLayer(self, n, input):
+        tmp_layer = self.Layer(n, input, 'direct')
+        tmp_layer.process
+        self.layers.append(tmp_layer)
+
+    def setInput(self, input):
+        size = len(self.layers)
+        print('Old input :', self.getInput())
+        self.input = input
+        if size != 0:
+            self.layers[0].input = input
+        print('New input :', self.getInput())
+
+    def getLayerList(self):
+        for i in self.layers:
+            print('%6s | %d Nodes'%(i.type, i.size))
+            for j in i.nodes:
+                print('%sInput :%s'%(''.ljust(7), j.input))
+                print('%sOutput :%s'%(''.ljust(10), j.output))
+            print('\n')
+    def getInput(self):
+        return self.input
+    def getOutput(self):
+        return self.layers[-1:][0].getOutput()
+
+    def allProcess(self):
+        print('\nBegin All process----------')
+        for i in self.layers:
+            print('Begin New Layer')
+            i.process()
+        print('End All process----------\n')
+
     # END NeuralNetwork class #
 
 if __name__ == '__main__':
     print('Hi')
     z = NeuralNetwork()
-    x = z.Layer(1,1)
-    i = [1, 2, 3, 4]
-    w = [-0.1, 0.3, -0.5, 0.1]
-    b = 0.4
-    n = x.LinearNode(i,w,b)
-    n.process()
-    print(n.getOutput())
-    n1 = x.ActivationNode(n.getOutput(), 'sig')
-    n1.process()
-    print(n1.getOutput())
+    # ip = [[1,1],[2,1],[4,0],[2,1],[5,5]]
+    ip = [0.01,0.39,0.41,0.22,0.6]
+    ip2 = [0]
+    # y = z.Layer(3, ip2)
+    # print('input',y.input)
+    # print(y.getOutput())
+    # y.process()
+    # out1 = y.getOutput()
+    # l2 = z.Layer(3, out1, 'direct')
+    # l2.process()
+    # out2 = l2.getOutput()
+    # l3 = z.Layer(2, out2)
+    # l3.process()
+    # out3 = l3.getOutput()
+    # print('out1', out1)
+    # print('out2', out2)
+    # print('out3', out3)
+    # z.addHidden(10)
+    z.addHidden(5)
+    z.addHidden(2)
+    z.getLayerList()
+    z.allProcess()
+    print(z.getOutput())
+    z.setInput(ip)
+    z.allProcess()
+    z.getLayerList()
+    print(z.getOutput())
