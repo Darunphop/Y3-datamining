65bd5d3b4204034b98af399d4275a3596a38d365
ICQCQ <darunphop.p@gmail.com>
2018-04-19 13:43:12 +0700

    main template

--- dm/classification/main.py
+++ dm/classification/main.py
@@ -2,4 +2,9 @@ import input
 import neuralNetwork
 
 if __name__ == '__main__':
-    pass
+    raw_data = input.loadFile().sample(frac=1)   #Load and shuffle data
+    kf_data = input.kFold(10, raw_data)
+    # print(len(kf_data))
+    for n, i in enumerate(kf_data):
+        print('%2d   ::   Train size : %d | Test size : %d'%(n+1, len(i[0]), len(i[1])))
+        print('%s'%(''.ljust(50,'-')))
