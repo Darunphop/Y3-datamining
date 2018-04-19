5f51db7b8213f8f9ecaaa98739026f6e65331e0a
ICQCQ <darunphop.p@gmail.com>
2018-04-19 16:19:59 +0700

    added founding

--- dm/classification/main.py
+++ dm/classification/main.py
@@ -5,13 +5,20 @@ if __name__ == '__main__':
     raw_data = input.loadFile().sample(frac=1)   #Load and shuffle data
     kf_data = input.kFold(10, raw_data)
     # print(len(kf_data))
-    for n, i in enumerate(kf_data):
+    for n, i in enumerate(kf_data): #each fold
         if 0 <= n < 1:
             print('%2d   ::   Train size : %d | Test size : %d'%(n+1, len(i[0]), len(i[1])))
             print('%s'%(''.ljust(50,'-')))
 
+            nn = neuralNetwork.NeuralNetwork(len(i[0])-1)
+            nn.addHidden(10)
+            nn.addHidden(5)
+            nn.addHidden(2)
+            nn.addHidden(1)
+            nn.getLayerList()
+            
             input.normalize(i[0])
             for j in range(len(i[0])):
                 inp = list(i[0].iloc[j][:-1])
-                print(inp)
-                print(j)
+                # print(inp)
+                # print(j)
